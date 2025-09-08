cf2efefdda96da16143b4c115843c59890374b43
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-08 10:56:14 +0530

    Tuples Added

--- Learning/Day_1/List.py
+++ Learning/Day_1/List.py
@@ -1,28 +1,33 @@
-# List in Python
-# Lists are ordered collections of items that can be of different types.
-# They are mutable, meaning their contents can be changed after creation.
+# # List in Python
+# # Lists are ordered collections of items that can be of different types.
+# # They are mutable, meaning their contents can be changed after creation.
 
-my_list = [1, 2, 3, 'four', 'five']
-print(my_list)
+# my_list = [1, 2, 3, 'four', 'five']
+# print(my_list)
 
-# Append in python is used with lists to add a single element to the end of the list.
-my_list.append(6)
-print(my_list)  # Output: [1, 2, 3, 'four', 'five', 6]
+# # Append in python is used with lists to add a single element to the end of the list.
+# my_list.append(6)
+# print(my_list)  # Output: [1, 2, 3, 'four', 'five', 6]
 
-# Insert in python is used with lists to add an element at a specific position.
-my_list.insert(2, 'three')
-print(my_list)  # Output: [1, 2, 'three', 3, 'four', 'five', 6]
+# # Insert in python is used with lists to add an element at a specific position.
+# my_list.insert(2, 'three')
+# print(my_list)  # Output: [1, 2, 'three', 3, 'four', 'five', 6]
 
-# Extend in python is used with lists to add multiple elements to the end of the list.
-lst1 = [1, 2, 3]
-lst2 = [4, 5, 6]
-lst1.extend(lst2)
-print(lst1)  # Output: [1, 2, 3, 4, 5, 6]
+# # Extend in python is used with lists to add multiple elements to the end of the list.
+# lst1 = [1, 2, 3]
+# lst2 = [4, 5, 6]
+# lst1.extend(lst2)
+# print(lst1)  # Output: [1, 2, 3, 4, 5, 6]
 
-Empty_List = [40, 70, 80, 90, 77]
-num = sum(Empty_List)
-# for i in Empty_List:
-#     num += i
+# Empty_List = [40, 70, 80, 90, 77]
+# num = sum(Empty_List)
+# # for i in Empty_List:
+# #     num += i
 
-avg = num/len(Empty_List)
-print("Average of List is:", avg)
+# avg = num/len(Empty_List)
+# print("Average of List is:", avg)
+
+
+# Define a single value tuple
+a = (5,)
+print(type(a))
---
+++ Learning/Day_1/tuple.py
@@ -0,0 +1,10 @@
+# Tuples in Python
+# Tuples are ordered collections of items that can be of different types.
+# They are immutable, meaning their contents cannot be changed after creation.
+
+my_tuple = (1, 2, 3, 'four', 'five')
+print(my_tuple)
+
+# Define a single value tuple
+a = (5,)
+print(type(a))  # Output: <class 'tuple'>
