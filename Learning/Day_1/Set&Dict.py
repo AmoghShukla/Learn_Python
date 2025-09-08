ccfaf2372112f85e26dbf3689c30181df71094fb
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-08 11:30:47 +0530

    Set&Dict Extended

--- Learning/Day_1/Set&Dict.py
+++ Learning/Day_1/Set&Dict.py
@@ -1,4 +1,60 @@
-a = set()
-b = {}
-print(type(a))
-print(type(b))
+# Difference Bewteen Set and Dictionary in Python
+# Set is an unordered collection of unique elements, 
+# while a dictionary is an unordered collection of key-value pairs.
+# Sets are defined using curly braces {} or the set() function,
+# whereas dictionaries are defined using curly braces {} with key-value pairs separated by colons.
+
+# a = set()
+# b = {}
+# print(type(a))
+# print(type(b))
+
+x = {'Vaishnavi', 1, 2, 3, 4, 5, 'Mayur'}
+x.pop()
+print(x)
+
+x.pop()
+print(x)
+
+x.pop()
+print(x)
+
+x.pop()
+print(x)
+
+x.pop()
+print(x)
+
+x.pop()
+print(x)
+
+x.pop()
+print(x)
+
+# Output:
+# {1, 2, 3, 4, 5, 'Mayur'}
+# {2, 3, 4, 5, 'Mayur'}
+# {3, 4, 5, 'Mayur'}
+# {4, 5, 'Mayur'}
+# {5, 'Mayur'}
+# {'Mayur'}
+# set()
+
+
+# Remove and Discard in Sets
+
+# Remove in python is used with sets to remove a specific element from the set.
+# If the element is not found, it raises a KeyError.
+
+# Discard in python is used with sets to remove a specific element from the set.
+# If the element is not found, it does nothing and does not raise an error.
+
+a = {1, 2, 3, 4, 5}
+a.remove(3)
+print(a)  # Output: {1, 2, 4, 5}
+
+a.discard(4)
+print(a)  # Output: {1, 2, 5}
+
+a.discard(10)  # Does nothing, no error
+print(a)  # Output: {1, 2, 5}
--- Learning/Day_1/Strings.py
+++ Learning/Day_1/Strings.py
@@ -4,4 +4,5 @@
 
 s1 = 'Hello'
 s2 = "World"
-print(s1 + s2)
+print(s1 + s2)  # Concatenation
+
