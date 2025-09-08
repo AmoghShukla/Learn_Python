f2e9ac697c227b3b3334922ff0215e95c1ef1bd0
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-08 11:08:22 +0530

    Index Added

---
+++ Learning/Day_1/Index.py
@@ -0,0 +1,7 @@
+# Index in Python
+# An index is a numerical representation of the position of an element within an ordered collection, such as a list or a string.
+# Indexing in Python starts from 0, meaning the first element is at index 0, the second element is at index 1, and so on.
+
+# Show position of an element in a list
+a = (1,2,3,4,5,6,7)
+print(a.index(5))  # Output: 4
