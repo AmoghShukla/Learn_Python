23a6546d508bdddeb659fcedd30db894f5b8da26
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-08 14:02:29 +0530

    Sample Programs Added in If Else If

--- Learning/Day_1/Conditional_Statements/if_elif_else.py
+++ Learning/Day_1/Conditional_Statements/if_elif_else.py
@@ -1,38 +1,55 @@
-# # If-Else Statements in Python
-# # If-Else statements are used for conditional execution of code blocks.
+# # # If-Else Statements in Python
+# # # If-Else statements are used for conditional execution of code blocks.
 
-age = 18
+# age = 18
 
-if age >= 18:
-    print("You are eligible to vote.")
+# if age >= 18:
+#     print("You are eligible to vote.")
+# else:
+#     print("You are not eligible to vote.")
+# # Output: You are eligible to vote.'
+
+# # # If-Elif-Else Statements in Python
+# # # If-Elif-Else statements are used for multiple conditional checks.
+
+# age = 18
+
+# if age < 18:
+#     print("You are a minor.")
+# elif age >= 18 and age < 65:
+#     print("You are an adult.")
+# else:
+#     print("You are a senior citizen.")
+# # Output: You are an adult.
+
+
+# # Traffic Light Example
+# # This example demonstrates the use of if-elif-else statements to determine actions based on traffic light colors. 
+# traffic_light = input("Enter the Color of Light : ").lower()
+# if traffic_light == "red":
+#     print("Stop")
+# elif traffic_light == "yellow":
+#     print("Caution, Go Slow!!")
+# elif traffic_light == "green":
+#     print("Go")
+# else:
+#     print("Invalid color")
+
+# ATM Example
+# This example uses if-elif-else statements to simulate an ATM transaction based on user input
+
+balance  = 10000
+
+Withdrawl_Amount = int(input("Enter the Amount You want to Withdraw : "))
+
+if Withdrawl_Amount > 0 and Withdrawl_Amount % 100 == 0 and Withdrawl_Amount <= balance:
+    balance -= Withdrawl_Amount
+    print("You Withdrew Rs.", Withdrawl_Amount, "from your Account, Your New Balance is : ", balance, "Rs.")
+elif Withdrawl_Amount > 0 and Withdrawl_Amount % 100 == 0 and Withdrawl_Amount > balance:
+    balance -= Withdrawl_Amount
+    print("Insufficient Funds!!")
 else:
-    print("You are not eligible to vote.")
-# Output: You are eligible to vote.'
+    print("Please Enter the a Valid Amount!!")
 
-# # If-Elif-Else Statements in Python
-# # If-Elif-Else statements are used for multiple conditional checks.
-
-age = 18
-
-if age < 18:
-    print("You are a minor.")
-elif age >= 18 and age < 65:
-    print("You are an adult.")
-else:
-    print("You are a senior citizen.")
-# Output: You are an adult.
-
-
-# Traffic Light Example
-# This example demonstrates the use of if-elif-else statements to determine actions based on traffic light colors. 
-traffic_light = input("Enter the Color of Light : ").lower()
-if traffic_light == "red":
-    print("Stop")
-elif traffic_light == "yellow":
-    print("Caution, Go Slow!!")
-elif traffic_light == "green":
-    print("Go")
-else:
-    print("Invalid color")
 
 
