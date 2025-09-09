fe89dba20cb60cbd730a7331202bff6fa15a8676
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-09 11:41:09 +0530

    Few more interview Questions

--- Learning/Day_2/Interview.py
+++ Learning/Day_2/Interview.py
@@ -1,39 +1,39 @@
-# # Q1) Find the max number in a list without using built-in functions
+# Q1) Find the max number in a list without using built-in functions
 
-# def Max_num(List1):
-#     max_num = List1[0]
-#     for i in List1:
-#         if i > max_num:
-#             max_num = i
-#     return max_num
-# print("Maximum Number is : ",Max_num([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))  
+def Max_num(List1):
+    max_num = List1[0]
+    for i in List1:
+        if i > max_num:
+            max_num = i
+    return max_num
+print("Maximum Number is : ",Max_num([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))  
 
 
-# # Q2) Find the Second Max number in a list without using built-in functions
+# Q2) Find the Second Max number in a list without using built-in functions
 
-# def Second_Max_num(List1):
-#     max_num = List1[0]
-#     second_max = 0
-#     for i in List1:
-#         if i > max_num:
-#             second_max = max_num
-#             max_num = i
-#         elif max_num > i > second_max:
-#             second_max = i
-#     return second_max
-# print("Second_Maximum Number is : ",Second_Max_num([5,7,9,1,8]))  
+def Second_Max_num(List1):
+    max_num = List1[0]
+    second_max = 0
+    for i in List1:
+        if i > max_num:
+            second_max = max_num
+            max_num = i
+        elif max_num > i > second_max:
+            second_max = i
+    return second_max
+print("Second_Maximum Number is : ",Second_Max_num([5,7,9,1,8]))  
 
-# # Q3) Find Vowels and its count in a String
+# Q3) Find Vowels and its count in a String
 
-# def Vowels_count(string):
-#     vowels = 'aeiouAEIOU'
-#     count = 0
-#     for i in string:
-#         if i in vowels:
-#             count += 1
-#     return count
-# str = input("Enter the String : ")
-# print(Vowels_count(str))
+def Vowels_count(string):
+    vowels = 'aeiouAEIOU'
+    count = 0
+    for i in string:
+        if i in vowels:
+            count += 1
+    return count
+str = input("Enter the String : ")
+print(Vowels_count(str))
 
 # Q4) Reverse a String without using built-in functions
 
