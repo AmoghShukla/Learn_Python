11b52c7543f6559a5133d88d4520e9c38fa369fc
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-09 10:12:01 +0530

    Uncommented Necessary Portions of the functions.py file

--- Learning/Day_1/functions.py
+++ Learning/Day_1/functions.py
@@ -2,41 +2,41 @@
 # Functions are reusable blocks of code that perform a specific task.
 # They are defined using the def keyword, followed by the function name and parentheses.
 
-# def greet(name):
-#     print(f"Hello, {name}!")
+def greet(name):
+    print(f"Hello, {name}!")
 
-# greet('Amogh')
+greet('Amogh')
 
-# # 1) Factorial
+# 1) Factorial
 
-# def factoria(num):
-#     if num == 0 or num == 1:
-#         return 1
-#     else:
-#         return num * factoria(num-1)
+def factoria(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return num * factoria(num-1)
     
     
-# n = int(input("Find factorial of: "))
-# print(factoria(n)) 
+n = int(input("Find factorial of: "))
+print(factoria(n)) 
 
-# # q) How to Access a global variable inside a function
+# q) How to Access a global variable inside a function
 
-# x = 10  # Global variable
-# def print_global():
-#     a = x 
-#     a -= 5
-#     print(a)  # Accessing the global variable
-# print(x)
-# print_global()  # Output: 5
-# print(x)  # Output: 5
+x = 10  # Global variable
+def print_global():
+    a = x 
+    a -= 5
+    print(a)  # Accessing the global variable
+print(x)
+print_global()  # Output: 5
+print(x)  # Output: 5
 
-# # Note: If you want to modify a global variable inside a function, you need to use the global keyword.
+# Note: If you want to modify a global variable inside a function, you need to use the global keyword.
 
-# # Default Arguments
-# def greet(name="Guest"):
-#     print(f"Hello, {name}!")
-# greet()  # Output: Hello, Guest!
-# greet("Alice")  # Output: Hello, Alice!
+# Default Arguments
+def greet(name="Guest"):
+    print(f"Hello, {name}!")
+greet()  # Output: Hello, Guest!
+greet("Alice")  # Output: Hello, Alice!
 
 # Pallindrome Number
 def is_palindrome(num):
@@ -46,10 +46,3 @@ print(is_palindrome(n))
 
 
 
-
-
-
-
-
-def check_palindrome(n):
-    return list(str(n)) == list(str(n)[::-1]) # String because we can reverse a string using slicing [::-1] but we cannot reverse an integer
