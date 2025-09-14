0a641d7a4d70a7400395dd9d638a6aa8188b2737
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-14 20:44:02 +0530

    Sleection Sort COmpleted

--- Learning/Day_5/DSA/SelectionsSort.py
+++ Learning/Day_5/DSA/SelectionsSort.py
@@ -3,3 +3,16 @@
 # The algorithm maintains two subarrays in a given array:
 # 1) The subarray which is already sorted.
 # 2) The subarray which is unsorted.
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+arr = [64, 25, 12, 22, 11, 90]
+print("Sorted Array is :", selection_sort(arr))
