6509426883f6faf222bde3bf4f3e621b618d7928
Amogh Shukla <amoghshukla548@gmail.com>
2025-09-14 20:43:30 +0530

    Selections Sort Added

---
+++ Learning/Day_5/DSA/SelectionsSort.py
@@ -0,0 +1,5 @@
+# Selection Sort Algorithm Implementation in Python
+# Selection Sort is an in-place comparison sorting algorithm that divides the input list into two parts: a sorted and an unsorted region. It repeatedly selects the smallest (or largest) element from the unsorted region and moves it to the end of the sorted region.
+# The algorithm maintains two subarrays in a given array:
+# 1) The subarray which is already sorted.
+# 2) The subarray which is unsorted.
